945a37fe91c0c2e28d3689657b9e4e75ba540771
aluno <Aluno@DOSO027251.educacional.up>
2022-03-22 21:26:52 -0300

    CRUD Produtos - Adição do alterar, remover e listar

--- CRUD_Produtos/src/controllers/ProdutoController.ts
+++ CRUD_Produtos/src/controllers/ProdutoController.ts
@@ -5,23 +5,31 @@ const produtoRepository = new ProdutoRepository();
 
 export class ProdutoController{
     create(request: Request, response: Response) {
-
         const produtos = produtoRepository.create(request.body);
-
         response.
             status(201).
             json({ message: "Produto cadastrado", data : produtos});
     }
 
-    list(){
-
+    list(request: Request, response: Response){
+        const produtos = produtoRepository.list();
+        response.
+            status(200).
+            json({ message: "Listagem de produtos", data : produtos});
     }
 
-    update(){
-
+    update(request: Request, response: Response){
+        const produtos = produtoRepository.update(request.body);
+        response.
+            status(200).
+            json({ message: "Produto alterado", data : produtos});
     }
 
-    delete(){
-
+    delete(request: Request, response: Response){
+        const nome = request.params.nome;
+        const produtos = produtoRepository.remove(nome);        
+        response.
+            status(200).
+            json({ message: "Produto removido", data : produtos});
     }
 }
--- CRUD_Produtos/src/repositories/ProdutoRepository.ts
+++ CRUD_Produtos/src/repositories/ProdutoRepository.ts
@@ -9,4 +9,24 @@ export class ProdutoRepository{
         return produtos;
     }
 
+    list(): Produto[]{
+        return produtos;
+    }
+
+    remove(nome: string) : Produto[]{
+        const index = 
+            produtos.findIndex((produto) => produto.nome === nome);
+        produtos.splice(index, 1);
+        return produtos;
+    }
+
+    update(produtoAlterado: Produto) : Produto[]{
+        const index = 
+            produtos.findIndex((produto) => produto.nome === produtoAlterado.nome);
+        produtos[index] = produtoAlterado;
+        return produtos;
+    }
+
+
+
 }
